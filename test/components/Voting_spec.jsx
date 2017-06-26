a780ca4a5c076e72585d96bd4e8dbc28fa451b0c
Andrew.Easterling <Anddrew914@gmail.com>
2017-06-26 14:56:42 -0400

    Fixes a typo in the test

--- test/components/Voting_spec.jsx
+++ test/components/Voting_spec.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {
-  renderIntoDocument
-  scryRenderedDOMComponenentsWithTag
+  renderIntoDocument,
+  scryRenderedDOMComponentsWithTag
 } from 'react-addons-test-utils';
 import Voting from '../../src/components/Voting';
+import {expect} from 'chai';
 
 describe('Voting', () => {
 
